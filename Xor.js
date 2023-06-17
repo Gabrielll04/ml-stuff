bd4bb82472810bd043438d08aaa40c0a008542ea
GuimaSpace <viniciusguimaraescarneiro@gmail.com>
2023-06-17 14:43:56 -0300

    Finishing the neuron network for non-linear gates

--- Xor.js
+++ Xor.js
@@ -1,10 +1,12 @@
+//special thanks to Tsoding! This code has nine parameters. 
+
 class Network {
     constructor() {
         this._train_data = [
             {x1: 1, x2: 1, y: 0},
-            {x1: 1, x2: 0, y: 1},
-            {x1: 0, x2: 1, y: 1},
-            {x1: 0, x2: 0, y: 0} 
+            {x1: 1, x2: 0, y: 0},
+            {x1: 0, x2: 1, y: 0},
+            {x1: 0, x2: 0, y: 1} 
         ]
 
         this._Initialize()
@@ -29,28 +31,43 @@ class Network {
     Forward(m, x1, x2) {
         let a = this.Sigmoid(m.or_w1 * x1 + m.or_w2 * x2 + m.or_b)
         let b = this.Sigmoid(m.nand_w1 * x1 + m.nand_w2 * x2 + m.nand_b) // a and b are your first layer, watch the video Machine Learning in C (episode 1) by Tsoding for more!
-        return this.Sigmoid(a * m.and_w1 + b * m.and_w2 + m.and_b)
+        return this.Sigmoid(a * m.and_w1 + b * m.and_w2 + m.and_b) // adding non-linearity to the neural network
     }
 
-    rand_sort() {
+    RandSort() {
         return Math.random()
     }
 
-    rand_xor() {
+    RandXor() {
         let m = {}
-        m.or_w1 = this.rand_sort()
-        m.or_w2 = this.rand_sort()
-        m.or_b = this.rand_sort()
-        m.nand_w1 = this.rand_sort()
-        m.nand_w2 = this.rand_sort()
-        m.nand_b = this.rand_sort()
-        m.and_w1 = this.rand_sort()
-        m.and_w2 = this.rand_sort()
-        m.and_b = this.rand_sort()
+        m.or_w1 = this.RandSort()
+        m.or_w2 = this.RandSort()
+        m.or_b = this.RandSort()
+        m.nand_w1 = this.RandSort()
+        m.nand_w2 = this.RandSort()
+        m.nand_b = this.RandSort()
+        m.and_w1 = this.RandSort()
+        m.and_w2 = this.RandSort()
+        m.and_b = this.RandSort()
+
         return m
     }
 
-    finite_diff(m) {
+    Learn(m, g) {
+        m.or_w1 -= this._rate*g.or_w1
+        m.or_w2 -= this._rate*g.or_w2
+        m.or_b -= this._rate*g.or_b
+        m.nand_w1 -= this._rate*g.nand_w1
+        m.nand_w2 -= this._rate*g.nand_w2
+        m.nand_b -= this._rate*g.nand_b
+        m.and_w1 -= this._rate*g.and_w1
+        m.and_w2 -= this._rate*g.and_w2
+        m.and_b -= this._rate*g.and_b
+
+        return m
+    }
+
+    FiniteDiff(m) {
         let g = {} //gradient, is similar to m, but its values are given a diff proximation function
         let saved //this variable will store the originial value of the variables to modify temporarily
 
@@ -58,7 +75,7 @@ class Network {
 
         saved = m.or_w1 //in this case, we preserve the value of m.or_w1 before edit it
         m.or_w1 += this._eps //see? we are editing the variable
-        g.or_w1 = (this.Cost(m) - c)/this._eps
+        g.or_w1 = (this.Cost(m) - c) / this._eps
         m.or_w1 = saved
 
         saved = m.or_w2
@@ -106,10 +123,24 @@ class Network {
 
     _Initialize() {
         this._eps = 1e-1
-        const m = this.rand_xor()
-        console.log(`cost = ${this.Cost(m)}`)
-        console.log(`rand. model = ${JSON.stringify(m, null, 2)}`)
-        console.log(`gradient = ${JSON.stringify(this.finite_diff(m), null, 2)}`)
+        this._rate = 1e-1
+
+        let m = this.RandXor()
+
+        console.log(JSON.stringify(this.Cost(m)))
+        for (let i = 0; i < 200*1000; i++) {
+            let g = this.FiniteDiff(m)
+            m = this.Learn(m, g)
+            console.log(JSON.stringify(this.Cost(m)))
+        }
+
+        console.log("________________________________________")
+
+        for (let i = 0; i < 2; i++) {
+            for (let j = 0; j < 2; j++) {
+                console.log(`${i} ^ ${j} = ${this.Forward(m, i, j)}`)
+            }
+        }
     }
 }
 new Network()
